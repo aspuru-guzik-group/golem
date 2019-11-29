bdf3598580465b4ebf793f85efc3f121f9caf8ed
matteoaldeghi <matteoaldeghi@vectorinstitute.ai>
2019-11-29 18:49:15 -0500

    rm unused timeit function

--- src/golem/golem.py
+++ src/golem/golem.py
@@ -12,23 +12,6 @@ pyximport.install(
 from .convolution import convolute
 
 
-def timeit(method):
-    """Decorator to time methods.
-    """
-    def timed(*args, **kw):
-        ts = time.time()
-        result = method(*args, **kw)
-        te = time.time()
-        if 'log_time' in kw:
-            name = kw.get('log_name', method.__name__.upper())
-            kw['log_time'][name] = int((te - ts) * 1000)
-        else:
-            print('%r  %2.2f ms' % \
-                      (method.__name__, (te-ts) * 1000))
-        return result
-    return timed
-
-
 class Golem(object):
 
     def __init__(self, X, y, dims, distributions, scales, max_depth=None, beta=0):
