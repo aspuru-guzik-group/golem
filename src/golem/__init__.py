c9a71f9672e2da5bb8af1e1dbb48466cebda07a0
Matteo Aldeghi <maldegh@mpibpc.mpg.de>
2021-03-04 16:13:40 -0500

    use external c module

--- src/golem/__init__.py
+++ src/golem/__init__.py
@@ -2,12 +2,6 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-import pyximport
-import numpy as np
-pyximport.install(
-        setup_args={'include_dirs': np.get_include()},
-        reload_support=True)
-
 from .golem import Golem
 from .extensions import BaseDist, Delta, Normal, TruncatedNormal, FoldedNormal
 from .extensions import Uniform, TruncatedUniform, BoundedUniform, Gamma
